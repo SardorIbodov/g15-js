ba64a37fc03cd9f4d6715ba54ec2727979c0c73a
Sardor <sardoribodov07@gmail.com>
2023-10-30 12:17:54 +0500

    1. Introduction

---
+++ 1/index.js
@@ -0,0 +1,81 @@
+//! Variable => o'zgaruvchi
+// let son = 13214;
+// console.log(son); // o'zgaruvchi
+// console.log("son"); // text
+
+//! Matematik amallar
+// console.log(son + 10);
+// console.log(son - 10);
+// console.log(son * 10);
+// console.log(son / 10);
+
+//! Ma'lumot turlari (Data-type)
+//? Primitive (Oddiy)
+//! Number
+// let son1 = 1234;
+// let son2 = 123.4;
+// console.log(typeof son1);
+// console.log(typeof son2);
+//! String
+// let string1 = "Hello world";
+// let string2 = 'Hello world';
+// let a = 32;
+// let string3 = `Hello world ${a}`;
+// let string1 = "Hello world", string2 = "Hello world", string3 = "Hello world";
+// console.log(string1, string2, string3);
+// console.log(string1);
+// console.log(string2);
+// console.log(string3);
+//! Boolean
+//? true, false
+// console.log(true);
+// console.log(false);
+// console.log(2 > 1);
+// console.log(2 < 1);
+// console.log(3 === 4);
+// console.log(typeof true);
+//! Null
+// let empty = null;
+// console.log(empty);
+//! Undefined
+// let empty;
+// console.log(empty);
+//! BigInt
+// let bigNumber = 9007199254740992n;
+// console.log(bigNumber * 2n);
+// console.log(typeof bigNumber);
+//! Symbol
+// let symbol1 = Symbol("Hello");
+// let symbol2 = Symbol("Hello");
+// console.log(symbol1 === symbol2);
+
+//? Non-primitive (oddiy bo'lmagan)
+// Object ( 1) Array, 2) Function, 3) Object)
+//! Array
+// let son = 1;
+// let string = "salom";
+// let array = [1, "salom", 2, "xayr", null, undefined];
+// console.log(typeof array);
+//! Function
+// function sayHello() {
+// 	console.log("Hello");
+// }
+// sayHello()
+//! Object
+// let object = {
+//   name: "John",
+//   age: 25,
+//   job: "Frontend developer",
+// };
+// console.log(object);
+
+// let son = 10;
+//! 1
+// son = 20;
+// console.log(son);
+//! 2
+// son = son + 10; // son = 10 + 10
+// son = son - 10; // son = 10 + 10
+// son = son * 10; // son = 10 + 10
+// son = son / 10; // son = 10 + 10
+// console.log(son);
