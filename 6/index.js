11f1e1a99de7dc43eb898bd8e0743b5cd88656f8
Sardor <sardoribodov07@gmail.com>
2023-11-13 12:25:23 +0500

    6. Object | basic

---
+++ 6/index.js
@@ -0,0 +1,59 @@
+//! Objects
+// let firstPerson = "name";
+// let person = {
+//   name: "John",
+//   age: 30,
+//   job: "Developer",
+// 	"kuzining rangi": "ko'k",
+// };
+
+// Accessing
+//? 1
+// console.log(person.age);
+//? 2
+// console.log(person["kuzining rangi"]);
+// console.log(person[firstPerson]);
+
+// Deleting
+// console.log(person);
+// delete person["kuzining rangi"];
+// console.log(person);
+
+//! in
+// console.log("ages" in person);
+
+//! loop (for-in)
+// let person = {
+//   name: "John/",
+//   age: 30,
+//   job: "Developer",
+//   address: "Muqimiy st",
+//   phoneNumber: "+998907298426",
+//   "kuzining rangi": "ko'k",
+// };
+// person.name = "John";
+// console.log(person);
+// for(let key in person) {
+// 	console.log(person[key]);
+// }
+
+// let user = {
+// 	name: "Smith",
+// 	age: 20,
+// 	name: "Soxib"
+// }
+
+// console.log(user);
+
+//! Practice
+// let menu = {
+//   window: 200,
+//   height: 300,
+//   title: "My menu",
+// };
+
+// for (item in menu) {
+//   if (typeof menu[item] === "number") menu[item] = menu[item] * 2;
+// }
+
+// console.log(menu);
