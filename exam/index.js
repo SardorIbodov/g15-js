4ede2411ee57e79db29820abb983da3a8c35ba6e
Sardor <sardoribodov07@gmail.com>
2023-12-01 11:39:53 +0500

    Exam tasks

---
+++ exam/index.js
@@ -0,0 +1,50 @@
+//! 3
+// let object = {
+//   name: "webbrain",
+//   title: "IT center",
+// };
+// console.log(Object.entries(object)); // [["name", "webbrain"], ["title", "IT center"]]
+
+//! 5
+// let user = {
+//   name: "Webbrain",
+//   surname: "Academy",
+//   info: {
+//     name: "IT Center",
+//   },
+// };
+
+// let {name, surname, info: {name: newName}} = user;
+
+//! 6
+// let list = [
+//   { id: 1, name: "Eshmat", title: "CEO" },
+//   { id: 2, name: "Toshmat", title: "CTO" },
+//   { id: 3, name: "Eshmat", title: "CEO" },
+//   { id: 4, name: "Toshmat", title: "CTO" },
+//   { id: 4, name: "Toshmat", title: "CMO" },
+// ];
+
+// const getResult = () => {
+//   let result = {};
+//   list.forEach((value) => {
+// 		result[value.title] ? result[value.title].push(value) : result[value.title] = [value]
+//     console.log(result);
+//   });
+// };
+// getResult();
+
+//! 12
+// console.log(Math.trunc(Math.random() * 100) + 1);
+
+//! 13
+// const factorial = (n) => {
+//   let result = 1;
+//   for (let i = 1; i <= n; i++) {
+//     result *= i;
+//   }
+//   return result;
+// };
+// console.log(factorial(0));
+
+//! 15
